39cc8ce78ec6b85b4d52284c17d73759b41c91f9
github username <github email address>
2020-06-06 09:01:49 -0700

    adding image field

--- index.js
+++ index.js
@@ -13,17 +13,21 @@ function getEquipment(){
             <div equipment-id=${equipment.id}>
                 <h3>${equipment.attributes.name}</h3>
                 <ul>
+                    <img src="${equipment.attributes.image} alt="${equipment.attributes.name}">
+                    <br>
                     <li><strong>Inventory_Id#: </strong>${equipment.attributes.id}</li>
                     <li><strong>price/hour: </strong>${equipment.attributes.price_per_hour}</li>
                     <li><strong>price/day: </strong>${equipment.attributes.price_per_day}</li>
                     <li><strong>price/month: </strong>${equipment.attributes.price_per_month}</li>
                     <li><strong>available for pickup? </strong>${equipment.attributes.pickup}</li>
                     <li><strong>available for delivery?</strong>${equipment.attributes.delivery}</li>
-                    <li>Provided by: ${equipment.relationships.vendor.data.id}</li>
+                    <li>Provided by: ${equipment.attributes.vendor.business_name}</li>
                 <button equipment-id= ${equipment.id}>Edit</button>
+                <button equipment-id= ${equipment.id}>Rent</button>
                 </ul>
             </div>
             <br><br>`;
+            
 
             document.querySelector("#equipment-container").innerHTML += equipmentInformation;
         });
